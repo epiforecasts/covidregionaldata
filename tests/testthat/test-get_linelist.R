0774304a8579ac54ac9b6ba96a7bf67d220a7138
seabbs <s.e.abbott12@gmail.com>
2020-03-17 12:30:44 +0000

    dropped test

--- tests/testthat/test-get_linelist.R
+++ tests/testthat/test-get_linelist.R
@@ -2,11 +2,11 @@ context("get_linelist")
 
 
 
-
-
-test_that("Can retrieve the linelist without errors", {
-  data <- suppressWarnings(get_linelist())
-
-  expect_true(!is.null(data))
-})
-
+#
+#
+# test_that("Can retrieve the linelist without errors", {
+#   data <- suppressWarnings(get_linelist())
+#
+#   expect_true(!is.null(data))
+# })
+#
