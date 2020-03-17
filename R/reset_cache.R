091455d6d2b84bbd6abdbf35d747e7e480d774bc
seabbs <s.e.abbott12@gmail.com>
2020-03-17 12:34:12 +0000

    made cache data reset optional

--- R/reset_cache.R
+++ R/reset_cache.R
@@ -1,5 +1,6 @@
 #' Reset Cache and Update all Local Data
-#'
+#' @param refresh_data Logical defaults to `FALSE`. Should all data sources be refreshed once the cache has been
+#' removed.
 #' @return Null
 #' @export
 #' @importFrom memoise cache_filesystem
@@ -7,15 +8,20 @@
 #'
 #'## Code
 #'reset_cache
-reset_cache <- function() {
+reset_cache <- function(refresh_data = FALSE) {
 
   unlink("cache", recursive = TRUE)
 
   cache <- memoise::cache_filesystem(".cache")
 
-  tmp <- NCoVUtils::get_international_linelist()
+  if (refresh_data) {
+    tmp <- NCoVUtils::get_international_linelist()
 
-  tmp <- NCoVUtils::get_who_cases()
+    tmp <- NCoVUtils::get_who_cases()
+
+    tmp < -NCoVUtils::get_italy_regional_cases()
+
+  }
 
   return(invisible(NULL))
 }
