11d20bd13a55c978814db7e13934c6a4f0767f6a
seabbs <s.e.abbott12@gmail.com>
2020-03-20 18:55:07 +0000

    updated website docs

--- R/get_total_cases.R
+++ R/get_total_cases.R
@@ -1,29 +1,65 @@
 #' Get Total Cases by Country
 #'
+#' @param source Character String specifying data source for total cases "WHO", "ECDC", default: "WHO".
 #' @return Returns a data frame of variables along with their most recent value.
 #' @export
-#' @importFrom dplyr select contains slice mutate arrange desc
+#' @importFrom dplyr select contains slice mutate arrange desc filter
 #' @importFrom tibble as_tibble
 #' @examples
 #'
+#' ## Total cases sourced from the WHO
+#' get_total_cases(source = "WHO")
 #'
-#' get_total_cases()
-get_total_cases <- function() {
+#' ## Total cases sourced from ECDC
+#' get_total_cases(source = "ECDC)
+get_total_cases <- function(source = 'WHO') {
 
-  V1 <- NULL; cases <- NULL;
+  who_total_cases <- function(total_cases){
 
-  total_cases <- NCoVUtils::get_who_cases()
+    V1 <- NULL; cases <- NULL;
 
-  total_cases <- total_cases %>%
-    dplyr::select(!dplyr::contains("deaths"))
+    total_cases <- total_cases %>%
+      dplyr::select(!dplyr::contains("deaths"))
 
-  total_cases <- total_cases[nrow(total_cases), ] %>%
-    t() %>%
-    tibble::as_tibble(rownames = "country") %>%
-    dplyr::slice(-c(1:2))  %>%
-    dplyr::mutate(cases = as.numeric(V1)) %>%
-    dplyr::select(-V1) %>%
-    dplyr::arrange(dplyr::desc(cases))
+    total_cases <- total_cases[nrow(total_cases), ] %>%
+      t() %>%
+      tibble::as_tibble(rownames = "country") %>%
+      dplyr::slice(-c(1:2))  %>%
+      dplyr::mutate(cases = as.numeric(V1)) %>%
+      dplyr::select(-V1) %>%
+      dplyr::arrange(dplyr::desc(cases)) %>%
+      dplyr::filter(!grepl("Region", country)) %>%
+      dplyr::filter(!grepl("Global", country))
+
+
+    return(total_cases)
+
+  }
+
+  ecdc_total_cases <- function(total_cases){
+    total_cases <- total_cases %>%
+      dplyr::group_by(country) %>%
+      dplyr::summarise(cases = sum(cases)) %>%
+      dplyr::arrange(-cases)
+
+    return(total_cases)
+
+  }
+
+  if (source == 'WHO'){
+    total_cases <- NCoVUtils::get_who_cases()
+
+    total_cases <- who_total_cases(total_cases)
+
+  }else if (source == 'ECDC'){
+    total_cases <- NCoVUtils::get_ecdc_cases()
+
+    total_cases <- ecdc_total_cases(total_cases)
+
+  }else{
+    stop('Unknown Data Source. Current data sources: "WHO", "ECDC"')
+  }
 
   return(total_cases)
+
 }
