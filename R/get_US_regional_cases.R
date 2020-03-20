f68e66b6a006f2caf48b69b58c5e6428d6385ce2
Joel Hellewell <j.hellewell14@imperial.ac.uk>
2020-03-20 18:37:43 +0000

    Implemented pull request changes

--- R/get_US_regional_cases.R
+++ R/get_US_regional_cases.R
@@ -3,15 +3,24 @@
 #' @description Fetches daily COVID cases by state collated by the
 #' John Hopkins Centre for Systems Science and Engineering (JHU CSSE)
 #' Data is available at https://github.com/CSSEGISandData/COVID-19
-#' @return
+#' @return A data.frame of COVID cases by region in the US
 #' @export
 #' @importFrom memoise cache_filesystem memoise
 #' @importFrom dplyr filter select mutate arrange group_by n lag
 #' @importFrom tidyr gather
 #' @importFrom lubridate mdy
 #' @examples
+#' get_us_regional_cases
 #'
-get_US_regional_cases <- function(){
+#' \dontrun{
+#'
+#'  regions <- rnaturalearth::ne_states("United States of America", returnclass = "sf")
+#'
+#'  regions %>% dplyr::filter(name %in% cases$region) %>%
+#'  ggplot2::ggplot() + ggplot2::geom_sf()
+#'
+#' }
+get_us_regional_cases <- function(){
 
   path <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
 
@@ -22,8 +31,9 @@ get_US_regional_cases <- function(){
 
   cases <- mem_read(path)
 
-  cases <- cases %>% dplyr::filter(`Province/State` %in% state.name) %>%
-    dplyr::select(-`Country/Region`,-Lat,-Long, region = `Province/State`) %>%
+  cases <- cases %>%
+    dplyr::filter(`Province/State` %in% state.name) %>%
+    dplyr::select(-`Country/Region`, -Lat, -Long, region = `Province/State`) %>%
     tidyr::gather(key = "date", value = "total_cases", -region) %>%
     dplyr::mutate(date = lubridate::mdy(date)) %>%
     dplyr::arrange(date) %>%
@@ -37,12 +47,6 @@ get_US_regional_cases <- function(){
     dplyr::mutate(cases = ifelse(cases < 0 , 0, cases)) %>%
     dplyr::filter(region != "Grand Princess", region != "Diamond Princess") %>%
     dplyr::full_join(data.frame(region = state.name, region_code = state.abb), by = "region")
-
-  # Example plotting code
-  # regions <- rnaturalearth::ne_states("United States of America", returnclass = "sf")
-  #
-  # regions %>% dplyr::filter(name %in% cases$region) %>%
-  #   ggplot2::ggplot() + ggplot2::geom_sf()
 
   return(cases)
 }
