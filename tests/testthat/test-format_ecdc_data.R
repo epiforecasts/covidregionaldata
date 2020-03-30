b8da238a9ce0837d1a3084f495d7bf12c597109f
Joel Hellewell <j.hellewell14@imperial.ac.uk>
2020-03-30 13:52:28 +0100

    Updated checks

--- tests/testthat/test-format_ecdc_data.R
+++ tests/testthat/test-format_ecdc_data.R
@@ -1,8 +1,8 @@
 test_that("format_ecdc_data works as expected", {
 
   base <- format_ecdc_data(get_ecdc_cases())
-  expect_is(base, data.frame)
-  expect_is(base$date, Date)
+  expect_is(base, "data.frame")
+  expect_is(base$date, "Date")
   expect_true(sum(as.numeric(base$cases) < 0) == 0)
 
   base2 <- format_ecdc_data(get_ecdc_cases(countries = c("France", "Germany")))
