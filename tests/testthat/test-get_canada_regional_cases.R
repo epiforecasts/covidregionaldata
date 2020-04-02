acffc619b15fc2fe460bbd9f9d7167d24fe4cace
Hamish Gibbs <hamishgibbs@Hamishs-MBP.lan>
2020-04-02 14:26:52 +0100

    get provincial cases and deaths for Canada

---
+++ tests/testthat/test-get_canada_regional_cases.R
@@ -0,0 +1,16 @@
+test_that("get_canada_regional_cases cases works as expected", {
+  
+  base <- get_canada_regional_cases(out = 'timeseries')
+  expect_is(base, "data.frame")
+  expect_is(base$date, "Date")
+  expect_true(sum(as.numeric(base$cases_probable) < 0) == 0)
+  expect_true(sum(as.numeric(base$deaths) < 0) == 0)
+  expect_true(sum(as.numeric(base$cases_confirmed) < 0) == 0)
+  
+  base <- get_canada_regional_cases(out = 'total')
+  expect_is(base, "data.frame")
+  expect_true(sum(as.numeric(base$cases_probable) < 0) == 0)
+  expect_true(sum(as.numeric(base$deaths) < 0) == 0)
+  expect_true(sum(as.numeric(base$cases_confirmed) < 0) == 0)
+  
+})
