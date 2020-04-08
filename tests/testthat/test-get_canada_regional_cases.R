d04bb8ba26b88010452341aee4fb3a58edc52228
Hamish Gibbs <hamishgibbs@Hamishs-MBP.lan>
2020-04-08 16:17:29 +0100

    adding data source tests

--- tests/testthat/test-get_canada_regional_cases.R
+++ tests/testthat/test-get_canada_regional_cases.R
@@ -14,3 +14,15 @@ test_that("get_canada_regional_cases cases works as expected", {
   expect_true(sum(as.numeric(base$cases_confirmed) < 0) == 0)
   
 })
+
+test_that("get_canada_regional_cases data source is unchanged", {
+  
+  base <- readr::read_csv("https://health-infobase.canada.ca/src/data/covidLive/covid19.csv")
+  
+  expected_colnames = c("pruid", "prname", "prnameFR", "date", "numconf", "numprob", 
+                        "numdeaths", "numtotal", "numtested", "numrecover", "percentrecover", 
+                        "ratetested", "numtoday", "percentoday")
+  
+  sapply(expected_colnames, expect_colname, colnames = colnames(base))
+  
+})
