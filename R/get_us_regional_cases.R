f91ca129b9dbfe11e32f336257aba5ae6649b496
Hamish Gibbs <hamishgibbs@Hamishs-MBP.lan>
2020-04-10 12:38:15 +0100

    changes to usa cases

--- R/get_us_regional_cases.R
+++ R/get_us_regional_cases.R
@@ -8,7 +8,7 @@
 #' @return A data.frame of COVID cases by region in the US
 #' @export
 #' @importFrom memoise cache_filesystem memoise
-#' @importFrom dplyr select summarise arrange group_by
+#' @importFrom dplyr select summarise arrange group_by mutate ungroup 
 #' @importFrom readr read_csv
 #' @importFrom tidyr drop_na
 #' @examples
@@ -34,25 +34,32 @@ get_us_regional_cases <- function(level = 'state', out = 'timeseries'){
   if(!level %in% c('state', 'county')){
     stop('Unknown input. Please provide admin level: "state", "county". default = "state"')
   }
-
+  
   nyt_state_url <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
   nyt_county_url <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
-
+  
   ch <- memoise::cache_filesystem(".cache")
   
   mem_read <- memoise::memoise(readr::read_csv, cache = ch)
   
   get_nyt_state <- function(path, out){
     
-    cases <- mem_read(path)
+    cases <- mem_read(path) %>% 
+      dplyr::group_by(state) %>% 
+      dplyr::mutate(cases = c(0, diff(cases)),
+                    deaths = c(0, diff(deaths))) %>%
+      dplyr::mutate(cases = replace(cases, cases < 0 , 0),
+                    deaths = replace(deaths, deaths < 0 , 0)) %>% 
+      dplyr::ungroup()
+    
     
     if (out == 'total'){
       cases <- cases %>% 
         dplyr::select(-date) %>% 
         dplyr::group_by(state) %>% 
         dplyr::summarise(fips = unique(fips),
-                  cases = sum(cases, na.rm = TRUE),
-                  deaths = sum(deaths, na.rm = TRUE)) %>% 
+                         cases = sum(cases, na.rm = TRUE),
+                         deaths = sum(deaths, na.rm = TRUE)) %>% 
         dplyr::arrange(-cases)
     }
     
@@ -62,7 +69,14 @@ get_us_regional_cases <- function(level = 'state', out = 'timeseries'){
   
   get_nyt_county <- function(path, out){
     
-    cases <- mem_read(path)
+    cases <- mem_read(path) %>% 
+      dplyr::group_by(fips) %>% 
+      tidyr::drop_na(fips) %>% 
+      dplyr::mutate(cases = c(0, diff(cases)),
+                    deaths = c(0, diff(deaths))) %>% 
+      dplyr::mutate(cases = replace(cases, cases < 0 , 0),
+                    deaths = replace(deaths, deaths < 0 , 0)) %>% 
+      dplyr::ungroup()
     
     if (out == 'total'){
       cases <- cases %>%
@@ -70,9 +84,9 @@ get_us_regional_cases <- function(level = 'state', out = 'timeseries'){
         dplyr::select(-date) %>% 
         dplyr::group_by(fips) %>%
         dplyr::summarize(county = unique(county),
-                  state = unique(state),
-                  cases = sum(cases, na.rm = TRUE),
-                  deaths = sum(deaths, na.rm = TRUE)) %>% 
+                         state = unique(state),
+                         cases = sum(cases, na.rm = TRUE),
+                         deaths = sum(deaths, na.rm = TRUE)) %>% 
         dplyr::arrange(-cases)
     }
     
@@ -83,14 +97,12 @@ get_us_regional_cases <- function(level = 'state', out = 'timeseries'){
   
   if (level == 'state'){
     
-    cases <- get_nyt_state(nyt_state_url, out)
+    return(get_nyt_state(nyt_state_url, out))
     
   }else if(level == 'county'){
     
-    cases <- get_nyt_county(nyt_county_url, out)
+    return(get_nyt_county(nyt_county_url, out))
     
   }
-
-  return(cases)
   
 }
