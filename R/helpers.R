e502324c89f9d9aaab19f630361acc3ad6cb8ea4
Jonnie Bevan <jonnie.bevan@tessella.com>
2020-05-20 15:10:39 +0100

    Helper functions

---
+++ R/helpers.R
@@ -0,0 +1,114 @@
+#' Daily counts from data that is in cumulative form.
+#' @description Gets daily counts from data column that is in cumulative form.
+#' @param column A vector of numeric data (e.g. a data column) which corresponds to cumulative counts of a process
+#' @return A vector of numeric data which corresponds to daily counts
+get_daily_from_cumulative <- function(column) {
+  shifted <- c(0, diff(column))
+  return(shifted)
+}
+
+#' Add extra columns filled with NA values to a dataset.
+#' @description Adds extra columns filled with NAs to a dataset. This ensures that all datasets from the NCovUtils package return datasets
+#' of the same underlying structure (i.e. same columns).
+#' @param data a data.frame
+#' @return a data.frame with relevant NA columns added
+add_extra_na_cols <- function(data) {
+  expected_col_names <- c("date", "province", "cases_today", "cumulative_cases", "deaths_today", "cumulative_deaths",
+                          "recoveries_today", "cumulative_recoveries", "tests_today", "cumulative_tests", "hospitalisations_today",
+                          "cumulative_hospitalisations")
+
+  for (colname in expected_col_names) {
+    if (!(colname %in% colnames(data))) {
+      original_col_names <- colnames(data)
+      data$newCol <- rep(NA, dim(data)[1])
+      colnames(data) <- c(original_col_names, colname)
+    }
+  }
+  return(data)
+}
+
+#' Helper to rename the region column in each dataset to the correct name for each country.
+#' @description The package relies on column name 'region' during processing but this often isn't the most sensible name for the column
+#' (e.g. state makes more sense for USA). This simply renames the column as the final step in processing before returning data to the user.
+#' @param data a data.frame with a region column
+#' @param country a string with the country of interest
+#' @importFrom dplyr %>% rename
+#' @return a data.frame with the column renamed to a sensible name
+rename_region_column <- function(data, country) {
+  new_name <- switch(tolower(country),
+                     "canada" = "province",
+                     "afghanistan" = "province")
+
+  data <- data %>% dplyr::rename(!!new_name := region)
+  return(data)
+}
+
+#' Set negative data to 0
+#' @description Set data values to 0 if they are negative in a dataset. Data in the datasets should always be > 0.
+#' @param data a data.frame
+#' @importFrom dplyr %>% mutate
+#' @return a data.frame with all relevant data > 0.
+set_negative_values_to_zero <- function(data) {
+  data <- data %>% dplyr::mutate(cumulative_deaths = replace(cumulative_deaths, cumulative_deaths < 0, 0),
+                                 cumulative_cases = replace(cumulative_cases, cumulative_cases < 0, 0),
+                                 cumulative_recoveries = replace(cumulative_recoveries, cumulative_recoveries < 0, 0),
+                                 cumulative_hospitalisations = replace(cumulative_hospitalisations, cumulative_hospitalisations < 0, 0),
+                                 cumulative_tests = replace(cumulative_tests, cumulative_tests < 0, 0),
+                                 cases_today = replace(cases_today, cases_today < 0, 0),
+                                 deaths_today = replace(deaths_today, deaths_today < 0, 0),
+                                 recoveries_today = replace(recoveries_today, recoveries_today < 0, 0),
+                                 hospitalisations_today = replace(hospitalisations_today, hospitalisations_today < 0, 0),
+                                 tests_today = replace(tests_today, tests_today < 0, 0))
+  return(data)
+}
+
+#' Add rows of NAs for dates where a region does not have any data
+#' @description THere are points, particularly early during data collection, where data was not collected for all regions. This function finds dates which have data for some
+#' regions, but not all, and adds rows of NAs for the missing regions. This is mainly for reasons of completeness.
+#' @param data a data.frame
+#' @importFrom dplyr %>%
+#' @importFrom tidyr complete full_seq
+#' @return a data.frame with rows of NAs added.
+fill_empty_dates_with_na <- function(data) {
+  data <- data %>%
+    tidyr::complete(date = tidyr::full_seq(data$date, period = 1),
+                    region,
+                    fill = list(
+                      cases_today = NA,
+                      cumulative_cases = NA,
+                      deaths_today = NA,
+                      cumulative_deaths = NA,
+                      recoveries_today = NA,
+                      cumulative_recoveries = NA,
+                      hospitalisations_today = NA,
+                      cumulative_hospitalisations = NA,
+                      tests_today = NA,
+                      cumulative_tests = NA)
+                    )
+  return(data)
+}
+
+#' Completes cumulative columns if rows were added with NAs.
+#' @description If a dataset had a row of NAs added to it (using fill_empty_dates_with_na) then cumulative data columns will have NAs which can cause
+#' issues later. This function fills these values with the previous non-NA value.
+#' @param data a data.frame
+#' @importFrom dplyr %>% group_by
+#' @importFrom tidyr fill
+#' @return a data.frame with NAs filled in for cumulative data columns.
+complete_cumulative_columns <- function(data) {
+  data <- data %>%
+    dplyr::group_by(region) %>%
+    tidyr::fill(cumulative_cases,
+                cumulative_deaths,
+                cumulative_recoveries,
+                cumulative_hospitalisations,
+                cumulative_tests)
+  return(data)
+}
+
+
+
+
+
+
+
