571ad004a44c2caf36c61c3bdcc67b7f59deb2d2
Jonnie Bevan <jonnie.bevan@tessella.com>
2020-06-11 14:10:19 +0100

    Roxygenise / documentation

--- R/covid19R-wrappers.R
+++ R/covid19R-wrappers.R
@@ -1,39 +1,80 @@
+#### These functions are exported and used to meet the Covid19R standard https://covid19r.github.io/documentation/standardized-package-functions.html
+#### They are essentially wrappers around get_long_format_regional_covid_data
+
+#' Get daily Afghan COVID-19 count data by Province (Wilåyat)
+#' @description Fetches  COVID-19 count data, stratified by date and province.
+#' Data sourced from https://docs.google.com/spreadsheets/d/1F-AMEDtqK78EA6LYME2oOsWQsgJi4CT3V_G4Uo-47Rg/export?format=csv.
+#' @return A tibble of COVID cases by province in Afghanistan.
+#' @export
 refresh_ncovutils_afghanistan <- function() {
   data <- get_long_format_regional_covid_data("afghanistan")
   return(data)
 }
 
+#' Get daily Belgian COVID-19 count data by Region
+#' @description Fetches COVID-19 count data, stratified by date and region
+#' Data sourced from https://epistat.sciensano.be/Data (3 different datasets - MORT, AGE_SEX and HOSP used)
+#' @return A tibble of COVID cases by province in Belgium.
+#' @export
 refresh_ncovutils_belgium <- function() {
   data <- get_long_format_regional_covid_data("belgium")
   return(data)
 }
 
+#' Get daily Brazilian COVID-19 count data by State (Estado)
+#' @description Fetches COVID-19 count data, stratified by date and state
+#' Data sourced from https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-cities-time.csv.
+#' @return A tibble of COVID cases by province in Brazil
+#' @export
 refresh_ncovutils_brazil <- function() {
   data <- get_long_format_regional_covid_data("brazil")
   return(data)
 }
 
+#' Get daily Canada COVID-19 count data by Province/Territory
+#' @description Fetches COVID-19 count data, stratified by date and province.
+#' Data sourced from https://health-infobase.canada.ca/src/data/covidLive/covid19.csv.
+#' @return A tibble of COVID cases by province in Canada.
+#' @export
 refresh_ncovutils_canada<- function() {
   data <- get_long_format_regional_covid_data("canada")
   return(data)
 }
 
+#' Get daily German COVID-19 count data by State (Bundesland)
+#' @description Fetches  COVID-19 count data, stratified by date and state.
+#' Data sourced from https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv.
+#' @return A tibble of COVID cases by province in Germany.
+#' @export
 refresh_ncovutils_germany <- function() {
   data <- get_long_format_regional_covid_data("germany")
   return(data)
 }
 
+#' Get daily Indian COVID-19 count data by State/Unified Territory
+#' @description Fetches COVID-19 count data, stratified by date and state.
+#' Data sourced from https://api.covid19india.org/csv/latest/state_wise_daily.csv.
+#' @return A tibble of COVID cases by province in India
+#' @export
 refresh_ncovutils_india <- function() {
   data <- get_long_format_regional_covid_data("india")
   return(data)
 }
 
+#' Get daily Italian COVID-19 count data by Region (Regioni).
+#' @description Fetches  COVID-19 count data, stratified by date and region.
+#' Data sourced from https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-<date>.csv.
+#' @return A tibble of COVID cases by province in Italy.
+#' @export
 refresh_ncovutils_italy <- function() {
   data <- get_long_format_regional_covid_data("italy")
   return(data)
 }
 
-
+#' Get meta information about the NCovUtils refresh_* data getters.
+#' @description Find out function names, data returned, subregion level and raw data sources, alongside package info such as licence details.
+#' @return A tibble of metadata about NCoVUtils
+#' @export
 get_info_ncovutils <- function() {
   tibble::tribble(
     ~data_set_name, ~package_name, ~function_to_get_data,
@@ -112,7 +153,7 @@ get_info_ncovutils <- function() {
     "ncovutils_italy",
     "ncovutils",
     "refresh_ncovutils_italy",
-    "Daily Covid-19 count data for regions (Regioni) of Afghanistan",
+    "Daily Covid-19 count data for regions (Regioni) of Italy",
     "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-<date>.csv",
     "https://github.com/epiforecasts/NCoVUtils/blob/master/LICENSE",
     "cases_new, cases_total, deaths_new, deaths_total, recovered_new, recovered_total, hosp_new, tested_total",
