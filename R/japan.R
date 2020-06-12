7ccd5cbb7521696696a2b1ca77b0ba71d8a0410c
Jonnie Bevan <jonnie.bevan@tessella.com>
2020-06-12 15:18:19 +0100

    Don't change Japan yet 2

--- R/japan.R
+++ R/japan.R
@@ -1,3 +1,9 @@
+#' Japan regional cases, daily
+
+#' @description Extracts regional case counts for Japan.
+#' [Source](https://mhlw-gis.maps.arcgis.com/apps/opsdashboard/index.html#/0c5d0502bbb54f9a8dddebca003631b8).
+#' @importFrom jsonlite fromJSON
+#' @importFrom rvest html_nodes html_text html_table
 #' @importFrom tidyr replace_na
 #' @importFrom dplyr mutate filter select group_by summarise n full_join
 #' @importFrom lubridate as_datetime
@@ -48,10 +54,8 @@ get_japan_regional_cases <- function(){
   #  print('This data source has changed. We are currently working to fix it.')
   #  return(tibble())
   
-  
   # Locate source
   location <- "https://services8.arcgis.com/JdxivnCyd1rvJTrY/arcgis/rest/services/covid19_list_csv_EnglishView/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=No%20desc%2C%E7%A2%BA%E5%AE%9A%E6%97%A5%20desc&resultOffset=0&resultRecordCount=2000&cacheHint=true"
-  location <- "https://services8.arcgis.com/JdxivnCyd1rvJTrY/arcgis/rest/services/covid19_list_csv_EnglishView/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=No%20desc%2C%E7%A2%BA%E5%AE%9A%E6%97%A5%20desc&resultOffset=0&cacheHint=true"
   # Set up cache
   ch <- memoise::cache_filesystem(".cache")
   mem_read <- memoise::memoise(jsonlite::fromJSON, cache = ch)
@@ -70,5 +74,6 @@ get_japan_regional_cases <- function(){
     dplyr::full_join(regions, by = c("region" = "value")) %>%
     dplyr::select(region, cases) %>%
     dplyr::mutate(cases = tidyr::replace_na(cases, 0))
+  
   return(data_df)
 }
