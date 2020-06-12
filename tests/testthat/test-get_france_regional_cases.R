f1b884353d1e1c3deaf9822c5705264be72b67ee
Jonnie Bevan <jonnie.bevan@tessella.com>
2020-06-12 13:52:02 +0100

    Delete france/UK-NHS regions files

--- tests/testthat/test-get_france_regional_cases.R
+++
@@ -1,8 +0,0 @@
-test_that("get_france_regional_cases works as expected", {
-
-  d <- get_france_regional_cases()
-  expect_is(d, "data.frame")
-  expect_is(d$date, "Date")
-  expect_is(d$cases, "integer")
-  expect_true(all(d$country == "France"))
-})
