24616697d6ce419e6d756492e46a36b524f2f4e9
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-08-18 12:02:57 +0100

    uk testthat
    simplified tests that work with UK API
    - api query is valid (ie returns status code 200)
    - data are up to date (<7 days)
    - correct number of regions returned at level 1 and 2
    - correct column types

--- tests/testthat/test-get_uk_regional_cases.R
+++ tests/testthat/test-get_uk_regional_cases.R
@@ -1,31 +1,24 @@
 test_that("get_uk_regional_cases data source is unchanged and up to date", {
   skip_on_cran()
+  api <- 'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=england&structure={"date":"date","areaName":"areaName","areaCode":"areaCode","newCasesByPublishDate":"newCasesByPublishDate","newDeaths28DaysByPublishDate":"newDeaths28DaysByPublishDate"}&format="csv'
+  get <- httr::GET(api)
+  last_modified <- stringr::str_sub(get[["headers"]][["last-modified"]], start = 6, end = -14)
   
-  eng_data <- readr::read_csv("https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cases_latest.csv")
-  eng_expected_colnames = c("Area name", "Area code", "Area type", "Specimen date", "Daily lab-confirmed cases",
-                        "Previously reported daily cases", "Change in daily cases", "Cumulative lab-confirmed cases", 
-                        "Previously reported cumulative cases", "Change in cumulative cases", "Cumulative lab-confirmed cases rate")
-  expect_true(all(eng_expected_colnames %in% colnames(eng_data)))
-  expect_true(max(lubridate::ymd(eng_data$`Specimen date`), na.rm = TRUE) > Sys.Date() - 7)
+  expect_lt(get$status_code, 399)
+  expect_true(lubridate::dmy(last_modified) > Sys.Date() - 7)
   
-  wales_scot_ni_data <- readr::read_csv("https://raw.githubusercontent.com/tomwhite/covid-19-uk-data/master/data/covid-19-cases-uk.csv")
-  wales_scot_ni_expected_colnames = c("Date", "Country", "AreaCode", "Area", "TotalCases")
-  expect_true(all(wales_scot_ni_expected_colnames %in% colnames(wales_scot_ni_data)))
-  expect_true(max(lubridate::ymd(wales_scot_ni_data$Date), na.rm = TRUE) > Sys.Date() - 7)
 })
 
 test_that("get_uk_regional_cases returns the correct column names", {
   skip_on_cran()
   
-  expected_colnames_adm_level_1 <- c("region_level_1", "date", "level_1_region_code", "cases_new", "cases_total")
+  expected_colnames_adm_level_1 <- c("region_level_1", "date", "level_1_region_code", "cases_new", "cases_total", "deaths_new", "deaths_total")
   returned_colnames_adm_level_1 <- colnames(get_uk_regional_cases_only_level_1())
-  expect_true(all(returned_colnames_adm_level_1 %in% expected_colnames_adm_level_1))
   expect_true(all(expected_colnames_adm_level_1 %in% returned_colnames_adm_level_1))
   
   
-  expected_colnames_adm_level_2 <- c("region_level_1", "region_level_2", "date", "iso_code", "level_2_region_code", "cases_new", "cases_total")
+  expected_colnames_adm_level_2 <- c("region_level_2", "date", "level_2_region_code", "cases_new", "cases_total", "deaths_new", "deaths_total")
   returned_colnames_adm_level_2 <- colnames(get_uk_regional_cases_with_level_2())
-  expect_true(all(returned_colnames_adm_level_2 %in% expected_colnames_adm_level_2))
   expect_true(all(expected_colnames_adm_level_2 %in% returned_colnames_adm_level_2))
 })
 
@@ -38,8 +31,6 @@ test_that("get_uk_regional_cases returns correct column types", {
   expect_is(data$region_level_1, "character")
   expect_is(data$date, "Date")
   expect_is(data$level_1_region_code, "character")
-  expect_is(data$cases_new, "numeric")
-  expect_is(data$cases_total, "numeric")
   
   
   data <- get_uk_regional_cases_with_level_2()
@@ -50,8 +41,6 @@ test_that("get_uk_regional_cases returns correct column types", {
   expect_is(data$date, "Date")
   expect_is(data$iso_code, "character")
   expect_is(data$level_2_region_code, "character")
-  expect_is(data$cases_new, "numeric")
-  expect_is(data$cases_total, "numeric")
 })
 
 test_that("get_uk_regional_cases returns correct numbers of regions", {
@@ -60,6 +49,6 @@ test_that("get_uk_regional_cases returns correct numbers of regions", {
   adm_1_data <- get_uk_regional_cases_only_level_1()
   adm_2_data <- get_uk_regional_cases_with_level_2()
   
-  expect_equal(length(unique(na.omit(adm_1_data$region_level_1))), 12)
-  expect_equal(length(unique(na.omit(adm_2_data$region_level_2))), 184)
+  expect_equal(length(unique(na.omit(adm_1_data$region_level_1))), 13)
+  expect_equal(length(unique(na.omit(adm_2_data$region_level_2))), 205)
 })
