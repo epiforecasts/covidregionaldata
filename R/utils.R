22af53988ab17c25d0dc77a7534a7aa1e421ba11
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2021-01-12 14:55:38 +0000

    docs

--- R/utils.R
+++ R/utils.R
@@ -254,4 +254,5 @@ utils::globalVariables(c(".", ":=", "AnzahlFall", "Area type", "Specimen date", 
                          "cases_new_na", "cases_total_na", "deaths_new_na", "deaths_total_na",
                          "ENTRY_DATE", "DATE_IMPLEMENTED",
                          "cases_weekly", "deaths_weekly",
-                         "fecha_confirmacion", "provincia", "get_data_function"))
+                         "fecha_confirmacion", "provincia", "get_data_function",
+                         "col_character"))
