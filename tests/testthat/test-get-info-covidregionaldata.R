06d96996b224b83cf2f60efba4c0f85986547153
kathsherratt <kathsherratt@gmail.com>
2021-02-24 10:25:46 +0000

    fix missing variable

--- tests/testthat/test-get-info-covidregionaldata.R
+++ tests/testthat/test-get-info-covidregionaldata.R
@@ -19,6 +19,7 @@ test_that("get_info_covidregionaldata includes data source for all countries", {
   countries <- get_info_covidregionaldata() %>%
     dplyr::filter(get_data_function == "get_regional_data" & !is.na(source_data_cols))
   
+  n_countries <- nrow(countries)
   n_url <- dplyr::pull(countries, data_url)
   
   expect_length(n_url, n_countries)
