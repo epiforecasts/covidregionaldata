e6612894ebebdef86428bb265f094a572e601f27
kathsherratt <kathsherratt@gmail.com>
2021-02-28 21:19:43 +0000

    fail safely
    - Add fail safe get-data function
    - Convert for loop into function to return better errors
    - Test only successfully returning datasets

--- tests/testthat/custom_tests/get_data.R
+++ tests/testthat/custom_tests/get_data.R
@@ -5,28 +5,43 @@
 # Get countries to test
 countries <- get_info_covidregionaldata() %>%
   dplyr::filter(get_data_function == "get_regional_data" & !is.na(source_data_cols))
+# Return failed attempts as NULL
+safely_get_regional_data <- purrr::possibly(get_regional_data, 
+                                            otherwise = NULL)
 
 # Admin level 1 data  -------------------------------------------
-
 countries_level_1 <- countries %>%
   dplyr::pull(country)
-
 data_level_1 <- purrr::map(countries_level_1, 
-                           ~ get_regional_data(country = .x, 
-                                               localise_regions = FALSE,
-                                               include_level_2_regions = FALSE))
+                           ~ safely_get_regional_data(country = .x, 
+                                                    localise_regions = FALSE,
+                                                    include_level_2_regions = FALSE))
 names(data_level_1) <- countries_level_1
-
+data_level_1 <- data_level_1 %>%
+  purrr::keep(~ !is.null(.x))
+success_level_1 <- names(data_level_1)
 
 # Admin level 2 data ------------------------------------------------------
-
 countries_level_2 <- countries %>%
   dplyr::filter(!is.na(level_2_region)) %>%
   dplyr::pull(country)
-
 data_level_2 <- purrr::map(countries_level_2, 
                            ~ get_regional_data(country = .x, 
                                                localise_regions = FALSE,
                                                include_level_2_regions = TRUE))
-names(data_level_2) <- countries_level_2
+names(data_level_2_all) <- countries_level_2
+data_level_2 <- data_level_2 %>%
+  purrr::keep(~ !is.null(.x))
+success_level_2 <- names(data_level_2)
+
+# Function for testing data types ------------------------------------------------------
+expect_data_type <- function(country, data_list, level = 1) {
+  expect_s3_class(data_list[[country]][["date"]], "Date")
+  expect_type(data_list[[country]][["cases_new"]], "double")
+  expect_type(data_list[[country]][["cases_total"]], "double")
+  expect_type(data_list[[country]][["region_level_1"]], "character")
+  if (level == 2) {
+    expect_type(data_list[[country]][["region_level_2"]], "character")
+  }
+}
 
--- tests/testthat/test-get-country-regional-data.R
+++ tests/testthat/test-get-country-regional-data.R
@@ -1,53 +1,32 @@
 # Tests each dataset returned by countries specified in get_info_covidregionaldata
 source("custom_tests/get_data.R")
 
+# Admin level 1 -----------------------------------------------------------
 # 1. Test data.frame is returned
-test_that("Level 1 data returned in dataframes", {
+test_that("All Level 1 countries return results", {
   skip_on_cran()
-  
-  level1_dataframes <- purrr::map(data_level_1, ~ class(.x)) %>%
-    purrr::keep(~ any(grepl("data.frame", .x))) %>%
-    length()
-  
-  expect_equal(level1_dataframes, length(countries_level_1))
+  expect_length(data_level_1, length(countries_level_1))
 })
-
 # 2. Test essential column types are appropriate
 test_that("Level 1 data has correct col types for date, region, cases", {
   skip_on_cran()
-  
-  for (country in countries_level_1) {
-    expect_s3_class(data_level_1[[country]][["date"]], "Date")
-    expect_type(data_level_1[[country]][["region_level_1"]], "character")
-    expect_type(data_level_1[[country]][["cases_new"]], "double")
-    expect_type(data_level_1[[country]][["cases_total"]], "double")
+  for (country in success_level_1) {
+    expect_data_type(country = country,
+                     data_list = data_level_1)
   }
 })
-  
-# Admin level 2 data -----------------------------------------------------------
-
-# Run tests:
-
+# Admin level 2 -----------------------------------------------------------
 # 1. 
-test_that("Level 2 data returns results in dataframes", {
+test_that("All Level 2 countries return results", {
   skip_on_cran()
-  
-  level2_dataframes <- purrr::map(data_level_2, ~ class(.x)) %>%
-    purrr::keep(~ any(grepl("data.frame", .x))) %>%
-    length()
-  
-  expect_equal(level2_dataframes, length(countries_level_2))
+  expect_length(data_level_2, length(countries_level_2))
 })
-
 # 2.
 test_that("Level 2 data has correct col types for date, region, and cases columns", {
   skip_on_cran()
-  
-  for (country in countries_level_2) {
-    expect_s3_class(data_level_2[[country]][["date"]], "Date")
-    expect_type(data_level_2[[country]][["region_level_1"]], "character")
-    expect_type(data_level_2[[country]][["region_level_2"]], "character")
-    expect_type(data_level_2[[country]][["cases_new"]], "double")
-    expect_type(data_level_2[[country]][["cases_total"]], "double")
+  for (country in success_level_2) {
+    expect_data_type(country = country,
+                     data_list = data_level_2,
+                     level = 2)
   }
 })
