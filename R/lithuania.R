e22406d033946858f030d55733714b0f0078b6c1
Richard Martin-Nielsen <richard.martin@utoronto.ca>
2021-03-04 20:03:17 +0200

    Merged in from master, ran styler, fixed WORDLIST again

--- R/lithuania.R
+++ R/lithuania.R
@@ -33,31 +33,44 @@ get_lithuania_regional_cases_only_level_1 <- function(national_data = FALSE,
   #                                      "\u0160iauliai County", "Taurag\u0117 County", "Tel\u0161iai County", "Utena County",
   #                                      "Vilnius County", "unstated"))
   #
-  county_data <- get_lithuania_regional_cases_with_level_2(national_data,
-                                                           all_osp_fields,
-                                                           death_definition) %>%
+  county_data <- get_lithuania_regional_cases_with_level_2(
+    national_data,
+    all_osp_fields,
+    death_definition
+  ) %>%
     dplyr::group_by(date, region_level_1) %>%
     dplyr::summarise(dplyr::across(tidyselect::vars_select_helpers$where(is.numeric), sum)) %>%
     dplyr::mutate(region_level_1 = if_else(is.na(.data$region_level_1),
-                                           "Lietuva", .data$region_level_1)) %>%
+      "Lietuva", .data$region_level_1
+    )) %>%
     dplyr::ungroup()
   # Fix percentages, where necessary
   if (all_osp_fields) {
     # For each of the test percentage fields, recalculate the percentages,
     # first checking that the total number of checks is not zero.
     county_data <- county_data %>%
-      dplyr::mutate(ab_prc_day =
-                      dplyr::if_else(.data$ab_tot_day == 0, 0,
-                                     .data$ab_pos_day / .data$ab_tot_day),
-                    ag_prc_day =
-                      dplyr::if_else(.data$ag_tot_day == 0, 0,
-                                     .data$ag_pos_day / .data$ag_tot_day),
-                    pcr_prc_day =
-                      dplyr::if_else(.data$pcr_tot_day == 0, 0,
-                                     .data$pcr_pos_day / .data$pcr_tot_day),
-                    dgn_prc_day =
-                      dplyr::if_else(.data$dgn_tot_day == 0, 0,
-                                     .data$dgn_pos_day / .data$dgn_tot_day))
+      dplyr::mutate(
+        ab_prc_day =
+          dplyr::if_else(
+            .data$ab_tot_day == 0, 0,
+            .data$ab_pos_day / .data$ab_tot_day
+          ),
+        ag_prc_day =
+          dplyr::if_else(
+            .data$ag_tot_day == 0, 0,
+            .data$ag_pos_day / .data$ag_tot_day
+          ),
+        pcr_prc_day =
+          dplyr::if_else(
+            .data$pcr_tot_day == 0, 0,
+            .data$pcr_pos_day / .data$pcr_tot_day
+          ),
+        dgn_prc_day =
+          dplyr::if_else(
+            .data$dgn_tot_day == 0, 0,
+            .data$dgn_pos_day / .data$dgn_tot_day
+          )
+      )
   }
   return(county_data)
 }
@@ -215,8 +228,7 @@ get_lithuania_regional_cases_only_level_1 <- function(national_data = FALSE,
 get_lithuania_regional_cases_with_level_2 <- function(national_data = FALSE,
                                                       all_osp_fields = FALSE,
                                                       death_definition = "of",
-                                                      recovered_definition = "official"
-                                                      ) {
+                                                      recovered_definition = "official") {
   # The following code, adjusted from a version for France, was initially used to
   # create lookup tables of Lithuanian municipality and country codes.
   # These were then adjusted to match the format used by the
@@ -228,74 +240,74 @@ get_lithuania_regional_cases_with_level_2 <- function(national_data = FALSE,
   #   rvest::html_nodes(xpath = '//*[@id=\"mw-content-text\"]/div/table') %>%
   #   rvest::html_table(fill = TRUE)
   municipality_county_lookup <- tibble::tribble(
-    ~region_level_2,          ~region_level_1,
-    "Akmen\u0117s r. sav.",      "\u0160iauli\u0173 apskritis",
-    "Alytaus m. sav.",      "Alytaus apskritis",
-    "Alytaus r. sav.",      "Alytaus apskritis",
-    "Anyk\u0161\u010di\u0173 r. sav.",       "Utenos apskritis",
-    "Bir\u0161tono sav.",        "Kauno apskritis",
-    "Bir\u017e\u0173 r. sav.",    "Panev\u0117\u017eio apskritis",
-    "Druskinink\u0173 sav.",      "Alytaus apskritis",
-    "Elektr\u0117n\u0173 sav.",     "Vilniaus apskritis",
-    "Ignalinos r. sav.",       "Utenos apskritis",
-    "Jonavos r. sav.",        "Kauno apskritis",
-    "Joni\u0161kio r. sav.",      "\u0160iauli\u0173 apskritis",
-    "Jurbarko r. sav.",     "Taurag\u0117s apskritis",
-    "Kai\u0161iadori\u0173 r. sav.",        "Kauno apskritis",
+    ~region_level_2, ~region_level_1,
+    "Akmen\u0117s r. sav.", "\u0160iauli\u0173 apskritis",
+    "Alytaus m. sav.", "Alytaus apskritis",
+    "Alytaus r. sav.", "Alytaus apskritis",
+    "Anyk\u0161\u010di\u0173 r. sav.", "Utenos apskritis",
+    "Bir\u0161tono sav.", "Kauno apskritis",
+    "Bir\u017e\u0173 r. sav.", "Panev\u0117\u017eio apskritis",
+    "Druskinink\u0173 sav.", "Alytaus apskritis",
+    "Elektr\u0117n\u0173 sav.", "Vilniaus apskritis",
+    "Ignalinos r. sav.", "Utenos apskritis",
+    "Jonavos r. sav.", "Kauno apskritis",
+    "Joni\u0161kio r. sav.", "\u0160iauli\u0173 apskritis",
+    "Jurbarko r. sav.", "Taurag\u0117s apskritis",
+    "Kai\u0161iadori\u0173 r. sav.", "Kauno apskritis",
     "Kalvarijos sav.", "Marijampol\u0117s apskritis",
-    "Kauno r. sav.",        "Kauno apskritis",
-    "Kauno m. sav.",        "Kauno apskritis",
+    "Kauno r. sav.", "Kauno apskritis",
+    "Kauno m. sav.", "Kauno apskritis",
     "Kazl\u0173 R\u016bdos sav.", "Marijampol\u0117s apskritis",
-    "K\u0117daini\u0173 r. sav.",        "Kauno apskritis",
-    "Kelm\u0117s r. sav.",      "\u0160iauli\u0173 apskritis",
-    "Klaip\u0117dos r. sav.",    "Klaip\u0117dos apskritis",
-    "Klaip\u0117dos m. sav.",    "Klaip\u0117dos apskritis",
-    "Kretingos r. sav.",    "Klaip\u0117dos apskritis",
-    "Kupi\u0161kio r. sav.",    "Panev\u0117\u017eio apskritis",
-    "Lazdij\u0173 r. sav.",      "Alytaus apskritis",
+    "K\u0117daini\u0173 r. sav.", "Kauno apskritis",
+    "Kelm\u0117s r. sav.", "\u0160iauli\u0173 apskritis",
+    "Klaip\u0117dos r. sav.", "Klaip\u0117dos apskritis",
+    "Klaip\u0117dos m. sav.", "Klaip\u0117dos apskritis",
+    "Kretingos r. sav.", "Klaip\u0117dos apskritis",
+    "Kupi\u0161kio r. sav.", "Panev\u0117\u017eio apskritis",
+    "Lazdij\u0173 r. sav.", "Alytaus apskritis",
     "Marijampol\u0117s sav.", "Marijampol\u0117s apskritis",
-    "Ma\u017eeiki\u0173 r. sav.",       "Tel\u0161i\u0173 apskritis",
-    "Mol\u0117t\u0173 r. sav.",       "Utenos apskritis",
-    "Neringos sav.",    "Klaip\u0117dos apskritis",
-    "Pag\u0117gi\u0173 sav.",     "Taurag\u0117s apskritis",
-    "Pakruojo r. sav.",      "\u0160iauli\u0173 apskritis",
-    "Palangos m. sav.",    "Klaip\u0117dos apskritis",
-    "Panev\u0117\u017eio m. sav.",    "Panev\u0117\u017eio apskritis",
-    "Panev\u0117\u017eio r. sav.",    "Panev\u0117\u017eio apskritis",
-    "Pasvalio r. sav.",    "Panev\u0117\u017eio apskritis",
-    "Plung\u0117s r. sav.",       "Tel\u0161i\u0173 apskritis",
-    "Prien\u0173 r. sav.",        "Kauno apskritis",
-    "Radvili\u0161kio r. sav.",      "\u0160iauli\u0173 apskritis",
-    "Raseini\u0173 r. sav.",        "Kauno apskritis",
-    "Rietavo sav.",       "Tel\u0161i\u0173 apskritis",
-    "Roki\u0161kio r. sav.",    "Panev\u0117\u017eio apskritis",
+    "Ma\u017eeiki\u0173 r. sav.", "Tel\u0161i\u0173 apskritis",
+    "Mol\u0117t\u0173 r. sav.", "Utenos apskritis",
+    "Neringos sav.", "Klaip\u0117dos apskritis",
+    "Pag\u0117gi\u0173 sav.", "Taurag\u0117s apskritis",
+    "Pakruojo r. sav.", "\u0160iauli\u0173 apskritis",
+    "Palangos m. sav.", "Klaip\u0117dos apskritis",
+    "Panev\u0117\u017eio m. sav.", "Panev\u0117\u017eio apskritis",
+    "Panev\u0117\u017eio r. sav.", "Panev\u0117\u017eio apskritis",
+    "Pasvalio r. sav.", "Panev\u0117\u017eio apskritis",
+    "Plung\u0117s r. sav.", "Tel\u0161i\u0173 apskritis",
+    "Prien\u0173 r. sav.", "Kauno apskritis",
+    "Radvili\u0161kio r. sav.", "\u0160iauli\u0173 apskritis",
+    "Raseini\u0173 r. sav.", "Kauno apskritis",
+    "Rietavo sav.", "Tel\u0161i\u0173 apskritis",
+    "Roki\u0161kio r. sav.", "Panev\u0117\u017eio apskritis",
     "\u0160aki\u0173 r. sav.", "Marijampol\u0117s apskritis",
-    "\u0160al\u010dinink\u0173 r. sav.",     "Vilniaus apskritis",
-    "\u0160iauli\u0173 r. sav.",      "\u0160iauli\u0173 apskritis",
-    "\u0160iauli\u0173 m. sav.",      "\u0160iauli\u0173 apskritis",
-    "\u0160ilal\u0117s r. sav.",     "Taurag\u0117s apskritis",
-    "\u0160ilut\u0117s r. sav.",    "Klaip\u0117dos apskritis",
-    "\u0160irvint\u0173 r. sav.",     "Vilniaus apskritis",
-    "Skuodo r. sav.",    "Klaip\u0117dos apskritis",
-    "\u0160ven\u010dioni\u0173 r. sav.",     "Vilniaus apskritis",
-    "Taurag\u0117s r. sav.",     "Taurag\u0117s apskritis",
-    "Tel\u0161i\u0173 r. sav.",       "Tel\u0161i\u0173 apskritis",
-    "Trak\u0173 r. sav.",     "Vilniaus apskritis",
-    "Ukmerg\u0117s r. sav.",     "Vilniaus apskritis",
-    "Utenos r. sav.",       "Utenos apskritis",
-    "Var\u0117nos r. sav.",      "Alytaus apskritis",
+    "\u0160al\u010dinink\u0173 r. sav.", "Vilniaus apskritis",
+    "\u0160iauli\u0173 r. sav.", "\u0160iauli\u0173 apskritis",
+    "\u0160iauli\u0173 m. sav.", "\u0160iauli\u0173 apskritis",
+    "\u0160ilal\u0117s r. sav.", "Taurag\u0117s apskritis",
+    "\u0160ilut\u0117s r. sav.", "Klaip\u0117dos apskritis",
+    "\u0160irvint\u0173 r. sav.", "Vilniaus apskritis",
+    "Skuodo r. sav.", "Klaip\u0117dos apskritis",
+    "\u0160ven\u010dioni\u0173 r. sav.", "Vilniaus apskritis",
+    "Taurag\u0117s r. sav.", "Taurag\u0117s apskritis",
+    "Tel\u0161i\u0173 r. sav.", "Tel\u0161i\u0173 apskritis",
+    "Trak\u0173 r. sav.", "Vilniaus apskritis",
+    "Ukmerg\u0117s r. sav.", "Vilniaus apskritis",
+    "Utenos r. sav.", "Utenos apskritis",
+    "Var\u0117nos r. sav.", "Alytaus apskritis",
     "Vilkavi\u0161kio r. sav.", "Marijampol\u0117s apskritis",
-    "Vilniaus m. sav.",     "Vilniaus apskritis",
-    "Vilniaus r. sav.",     "Vilniaus apskritis",
-    "Visagino sav.",       "Utenos apskritis",
-    "Zaras\u0173 r. sav.",       "Utenos apskritis",
-    "nenustatyta",            "nenustatyta",
-    "Unknown",                "Unknown"
+    "Vilniaus m. sav.", "Vilniaus apskritis",
+    "Vilniaus r. sav.", "Vilniaus apskritis",
+    "Visagino sav.", "Utenos apskritis",
+    "Zaras\u0173 r. sav.", "Utenos apskritis",
+    "nenustatyta", "nenustatyta",
+    "Unknown", "Unknown"
   )
-  
+
   # Advertise the fine documentation.
   message("Use ?get_lithuania_regional_cases_with_level_2() for more information on this dataset")
-  
+
   # Read data --------------------------------------------------------------------
   cases_url <- "https://opendata.arcgis.com/datasets/d49a63c934be4f65a93b6273785a8449_0.csv"
   osp_data <- csv_reader(file = cases_url)
@@ -305,108 +317,132 @@ get_lithuania_regional_cases_with_level_2 <- function(national_data = FALSE,
   #
   # death_definition : default is "of"
   death_field <- switch(death_definition,
-         of = "daily_deaths_def1",
-         daily_deaths_def1 = "daily_deaths_def1",
-         with = "daily_deaths_def2",
-         daily_deaths_def2 = "daily_deaths_def2",
-         after = "daily_deaths_def3",
-         daily_deaths_def3 = "daily_deaths_def3",
-         having_had = "daily_deaths_def3")
+    of = "daily_deaths_def1",
+    daily_deaths_def1 = "daily_deaths_def1",
+    with = "daily_deaths_def2",
+    daily_deaths_def2 = "daily_deaths_def2",
+    after = "daily_deaths_def3",
+    daily_deaths_def3 = "daily_deaths_def3",
+    having_had = "daily_deaths_def3"
+  )
   # If death_definition doesn't match one of our possibilities, it will
   # return NULL
   #
   if (is.null(death_field)) {
-    message(paste0("death_definition of \"", death_definition,
-                   "\" not recognised, defaulting to \"of\""))
+    message(paste0(
+      "death_definition of \"", death_definition,
+      "\" not recognised, defaulting to \"of\""
+    ))
     death_field <- "daily_deaths_def1"
   }
-  
+
   # recovered_definition : default is "official"
   recovered_field <- switch(recovered_definition,
-                        official = "recovered_de_jure",
-                        recovered_de_jure = "recovered_de_jure",
-                        de_jure = "recovered_de_jure",
-                        recovered_sttstcl = "recovered_sttstcl",
-                        statistical = "recovered_sttstcl",
-                        estimated = "recovered_sttstcl"
-                        )
+    official = "recovered_de_jure",
+    recovered_de_jure = "recovered_de_jure",
+    de_jure = "recovered_de_jure",
+    recovered_sttstcl = "recovered_sttstcl",
+    statistical = "recovered_sttstcl",
+    estimated = "recovered_sttstcl"
+  )
   # If recovered_definition doesn't match one of our possibilities, it will
   # return NULL
   #
   if (is.null(death_field)) {
-    message(paste0("recovered_definition of \"", recovered_definition,
-                   "\" not recognised, defaulting to \"official\""))
+    message(paste0(
+      "recovered_definition of \"", recovered_definition,
+      "\" not recognised, defaulting to \"official\""
+    ))
     recovered_field <- "recovered_de_jure"
   }
 
   # Build "unassigned" data by subtracting the aggregate from the countrywide
   # total provided for Lietuva
-  
+
   # Get relevant column names for differences (i.e. not percentages
   # or qualitative)
   sum_cols <- names(select(osp_data, "population":tidyselect::last_col()))
   sum_cols <- sum_cols[!grepl("prc|map_colors", sum_cols)]
-  
+
   # Take the difference between national and sum of counties' data
   unassigned <- osp_data %>%
     dplyr::mutate(national = ifelse(.data$municipality_name == "Lietuva", "national", "municipality")) %>%
     dplyr::group_by(date, .data$national) %>%
     dplyr::summarise(across(tidyselect::all_of(sum_cols), ~ sum(.x, na.rm = TRUE))) %>%
     dplyr::mutate(across(tidyselect::all_of(sum_cols), ~ dplyr::lead(.x, 1) - .x),
-                  municipality_name = "Unknown",
-                  ab_prc_day =
-                    dplyr::if_else(.data$ab_tot_day == 0, 0,
-                                   .data$ab_pos_day / .data$ab_tot_day),
-                  ag_prc_day =
-                    dplyr::if_else(.data$ag_tot_day == 0, 0,
-                                   .data$ag_pos_day / .data$ag_tot_day),
-                  pcr_prc_day =
-                    dplyr::if_else(.data$pcr_tot_day == 0, 0,
-                                   .data$pcr_pos_day / .data$pcr_tot_day),
-                  dgn_prc_day =
-                    dplyr::if_else(.data$dgn_tot_day == 0, 0,
-                                   .data$dgn_pos_day / .data$dgn_tot_day),
-                  map_colors = NA_character_) %>%
+      municipality_name = "Unknown",
+      ab_prc_day =
+        dplyr::if_else(
+          .data$ab_tot_day == 0, 0,
+          .data$ab_pos_day / .data$ab_tot_day
+        ),
+      ag_prc_day =
+        dplyr::if_else(
+          .data$ag_tot_day == 0, 0,
+          .data$ag_pos_day / .data$ag_tot_day
+        ),
+      pcr_prc_day =
+        dplyr::if_else(
+          .data$pcr_tot_day == 0, 0,
+          .data$pcr_pos_day / .data$pcr_tot_day
+        ),
+      dgn_prc_day =
+        dplyr::if_else(
+          .data$dgn_tot_day == 0, 0,
+          .data$dgn_pos_day / .data$dgn_tot_day
+        ),
+      map_colors = NA_character_
+    ) %>%
     dplyr::filter(.data$national == "municipality") %>%
     dplyr::select(-.data$national)
-  
+
   # Join unknown locations to main dataset
   osp_data_w_unassigned <- dplyr::bind_rows(
-    osp_data%>%select(-object_id,-municipality_code), unassigned)
-  
+    osp_data %>% select(-.data$object_id, -.data$municipality_code), unassigned
+  )
+
   # Exclude national data based on user param (default = FALSE)
   if (!national_data) {
-    osp_data_w_unassigned <- 
-      dplyr::filter(osp_data_w_unassigned,
-                    !.data$municipality_name == "Lietuva")
+    osp_data_w_unassigned <-
+      dplyr::filter(
+        osp_data_w_unassigned,
+        !.data$municipality_name == "Lietuva"
+      )
   }
-  
+
   cases_data <- osp_data_w_unassigned %>%
     dplyr::mutate(
       date = lubridate::as_date(date),
       tested_new = .data$ab_tot_day + .data$ag_tot_day + .data$pcr_tot_day,
       deaths_new = .data[[death_field]],
-      recovered_total = .data[[recovered_field]]) %>%
-    dplyr::rename(cases_new = .data$incidence,
-                  cases_total = .data$cumulative_totals,
-                  region_level_2 = .data$municipality_name) %>%
+      recovered_total = .data[[recovered_field]]
+    ) %>%
+    dplyr::rename(
+      cases_new = .data$incidence,
+      cases_total = .data$cumulative_totals,
+      region_level_2 = .data$municipality_name
+    ) %>%
     dplyr::left_join(municipality_county_lookup, by = c("region_level_2")) %>%
-    dplyr::select(date, region_level_1, region_level_2,
-                  cases_new, cases_total, deaths_new,
-                  tested_new, recovered_total,
-                  dplyr::everything())
+    dplyr::select(
+      date, region_level_1, region_level_2,
+      cases_new, cases_total, deaths_new,
+      tested_new, recovered_total,
+      dplyr::everything()
+    )
   # If we have not been asked to return all the OSP-provided data,
   # just select the core data sought by get_regional_data
   # (default = FALSE)
   if (!all_osp_fields) {
     cases_data <- cases_data %>%
-      dplyr::select(date, region_level_1, region_level_2,
-                    cases_new, cases_total, deaths_new, tested_new, recovered_total)
+      dplyr::select(
+        date, region_level_1, region_level_2,
+        cases_new, cases_total, deaths_new, tested_new, recovered_total
+      )
   }
-    ## This is the list of fields which we're trying to generate, copied from get_regional_data.R
-    # date, region_level_2, level_2_region_code, region_level_1, level_1_region_code,
-    # cases_new, cases_total, deaths_new, deaths_total,
-    # recovered_new, recovered_total, hosp_new, hosp_total,
-    # tested_new, tested_total, dplyr::everything())
+  ## This is the list of fields which we're trying to generate, copied from get_regional_data.R
+  # date, region_level_2, level_2_region_code, region_level_1, level_1_region_code,
+  # cases_new, cases_total, deaths_new, deaths_total,
+  # recovered_new, recovered_total, hosp_new, hosp_total,
+  # tested_new, tested_total, dplyr::everything())
   return(cases_data)
 }
