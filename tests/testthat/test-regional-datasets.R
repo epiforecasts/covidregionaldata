6a5679028638a778adbc84daa7c38a8caf8337c5
Sam Abbott <s.e.abbott12@gmail.com>
2021-03-07 19:41:42 +0000

    add control flow and documentation for testing

--- tests/testthat/test-regional-datasets.R
+++ tests/testthat/test-regional-datasets.R
@@ -1,14 +1,27 @@
+# load testing function and tools. 
+# set up custom tests using:
+# custom_tests/regional-dataset-specific.R
 source("custom_tests/test-regional-dataset.R")
 
-# test your custom dataset and add a data snapshot using 
-# test_regional_dataset(source = "region", level = c("1", "2"), download = TRUE)
-
-# loops over all regional datasets and tests
-download <- FALSE
-source_of_interest <- NULL
+# sources implemented currently (temporary)
 implemented_sources <- c("ecdc", "who", "mexico")
 
-# filter out target datasets for testing
+# should a single dataset be tested vs all datasets
+# set this when implementing a new dataset.
+# Can also be set using environment variables
+source_of_interest <- NULL
+if (!is.null(getOption("testSource"))) {
+    source_of_interest <- getOption("testSource")
+}
+# should downloads be tested (defaults to FALSE)
+# set this to true when implementing a new data set
+# can also be controlled using an environment variable
+download <- FALSE
+if (!is.null(getOption("testDownloads"))) {
+    download <- getOption("testDownloads")
+}
+
+# get datasets for testing
 sources <- covidregionaldata::available_datasets %>%
     filter(.data$get_data_function %in%
            c("get_regional_data", "get_national_data")) %>%
@@ -23,12 +36,19 @@ sources <- covidregionaldata::available_datasets %>%
     ) %>%
     tidyr::drop_na(regions)
 
+# filter out target datasets
+if (!is.null(source_of_interest)) {
+    sources <- sources %>%
+        dplyr::filter(source %in% source_of_interest)
+}
+
 # temporary - just test implemented datasets
 sources <- sources %>%
     dplyr::filter(source %in% implemented_sources) %>%
     dplyr::rowwise() %>%
     dplyr::group_split()
 
+# apply tests to each data source in turn
 purrr::walk(sources,
     ~ test_regional_dataset(
         source = .$source[[1]], level = .$level[[1]], download = download
