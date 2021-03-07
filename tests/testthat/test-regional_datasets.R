90125f1ce1a2a82f6c7548f83861b51add7d806a
Sam Abbott <s.e.abbott12@gmail.com>
2021-03-07 14:06:53 +0000

    add comments for testing

---
+++ tests/testthat/test-regional_datasets.R
@@ -0,0 +1,23 @@
+# should data be downloaded or snapshot used
+
+# is there a target dataset or should all datasets be tested
+
+# loop overall target datasets
+
+# loop over available levels
+
+# setup data class and test it can be found
+
+# test data download (does it download)
+
+# load stored data if present
+
+# compare stored with downloaded data
+
+# save new data snapshot
+
+# test cleaning of data
+
+# test processing of data
+
+# test data return
