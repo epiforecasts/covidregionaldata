a1ab8ec47238001c6ad0a3767f6ff021dec160fe
Sam Abbott <s.e.abbott12@gmail.com>
2021-03-07 22:39:01 +0000

    port in code as written

--- R/italy.R
+++ R/italy.R
@@ -13,7 +13,9 @@
 #' italy$raw
 #' }
 download_regional.crd_italy_1 <- function(region, verbose = TRUE, ...) {
-  region$raw <- NULL
+  base_url <- "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/"
+  url <- paste0(base_url, "dati-regioni/dpc-covid19-ita-regioni.csv")
+  region$raw <- suppressWarnings(csv_reader(url))
   return(region)
 }
 
@@ -23,6 +25,8 @@ download_regional.crd_italy_1 <- function(region, verbose = TRUE, ...) {
 #' @inheritParams clean_regional
 #' @method clean_regional crd_italy_1
 #' @author Sam Abbott
+#' @importFrom dplyr mutate select arrange recode group_by ungroup
+#' @importFrom lubridate as_date ymd_hms
 #' @examples
 #' \dontrun{
 #' italy <- new_covidregionaldata("italy")
@@ -30,6 +34,23 @@ download_regional.crd_italy_1 <- function(region, verbose = TRUE, ...) {
 #' clean_regional(italy)$clean
 #' }
 clean_regional.crd_italy_1 <- function(region, verbose = TRUE, ...) {
-  region$clean <- region$raw
+  region$clean <- region$raw %>%
+    mutate(
+      date = as_date(ymd_hms(.data$data)),
+      region_level_1 = as.character(.data$denominazione_regione),
+      cases_total = .data$totale_casi,
+      deaths_total = .data$deceduti,
+      tested_total = .data$tamponi
+    ) %>%
+    select(.data$date, .data$region_level_1, .data$cases_total,
+           .data$deaths_total, .data$tested_total) %>%
+    arrange(.data$date) %>%
+    mutate(region_level_1 = recode(.data$region_level_1,
+      "P.A. Trento" = "Trentino-Alto Adige",
+      "P.A. Bolzano" = "Trentino-Alto Adige"
+    )) %>%
+    group_by(.data$date, .data$region_level_1) %>%
+    mutate(cases_total = sum(.data$cases_total, na.rm = TRUE)) %>%
+    ungroup()
   return(region)
 }
