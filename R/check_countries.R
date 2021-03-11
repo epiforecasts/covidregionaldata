4fd7a88fa332e2fee5bc200fcaa944453e53c37e
Joe <palmer-2@hotmail.co.uk>
2021-03-11 20:39:58 +0000

    Added new function to list all countries by searching the environment for country classes. Can determine which has what level, but need to format into a dataframe.

--- R/check_countries.R
+++ R/check_countries.R
@@ -23,3 +23,17 @@ show_countries <- function() {
     )
   return(countries)
 }
+
+show_R6_classes <- function(){
+  envi <-ls("package:covidregionaldata")
+  starts_with_capitals_idx <- grep("^[A-Z][a-z]", envi)
+  starts_with_capitals <- envi[starts_with_capitals_idx]
+  exclude <- c("Who", "Ecdc", "NameOfCountry")
+  check <- lapply(starts_with_capitals, function(x) {class(get(x))=="R6ClassGenerator" & !(x %in% c(exclude))})
+  valid_country_classes <- starts_with_capitals[unlist(check)]
+  public_field_names <- lapply(valid_country_classes, function(x) {!(is.na(get(x)$public_fields["level_2_region"]))})
+  level_2_avaliable <- valid_country_classes[unlist(public_field_names)]
+  # Format the level 1 and level 2 into a dataframe like show countries.
+  
+  return(level_2_avaliable)
+}
