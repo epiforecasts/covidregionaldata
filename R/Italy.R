7aabcbb4119d0e488971b09b8d5a60a851f0c972
Joe <palmer-2@hotmail.co.uk>
2021-03-17 10:54:52 +0000

    Format with lintr.

--- R/Italy.R
+++ R/Italy.R
@@ -15,7 +15,7 @@ Italy <- R6::R6Class("italy",
     #' @field level_2_region the level 2 region name.
     level_2_region = NA,
     #' @field data_url link to raw data
-    data_url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv",
+    data_url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv", # nolint
     #' @field source_data_cols existing columns within the raw data
     source_data_cols = c("cases_total", "deaths_total", "tested_total"),
 
@@ -42,16 +42,20 @@ Italy <- R6::R6Class("italy",
         group_by(.data$date, .data$region_level_1) %>%
         mutate(cases_total = sum(.data$cases_total, na.rm = TRUE)) %>%
         ungroup() %>%
-        full_join(self$region$codes_lookup, by = c("region_level_1" = "region")) %>%
+        full_join(self$region$codes_lookup,
+          by = c("region_level_1" = "region")
+        ) %>%
         select(.data$date, .data$region_level_1,
           level_1_region_code = .data$code,
           .data$cases_total, .data$deaths_total, .data$tested_total
         )
     },
 
-    #' @description Set up the country class with attributes set to input parameters
+    #' @description Set up the country class with attributes set to input
+    #' parameters
     #' @param level The region level for the data
-    #' @param totals Boolean. If TRUE, returns totalled data per region up to today's date.
+    #' @param totals Boolean. If TRUE, returns totalled data per region up to
+    #' today's date.
     #' @param localise Boolean. Should region names be localised.
     #' @param verbose Boolean. Display information at various stages.
     #' @param steps Boolean. Keep data from each processing step.
