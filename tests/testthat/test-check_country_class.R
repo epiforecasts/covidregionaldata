1eaa274db476657e0016603d888146edec545c9a
Joe <palmer-2@hotmail.co.uk>
2021-03-23 15:03:44 +0000

    Added more general tests.

---
+++ tests/testthat/test-check_country_class.R
@@ -0,0 +1,51 @@
+check_country_fields <- function(regionClass) {
+  expected_public_fields <- names(CountryTemplate$public_fields)
+  region_public_fields <- names(regionClass$public_fields)
+  for (field in expected_public_fields) {
+    test_that(
+      paste("Expect", field, "is in", class(regionClass)[1], "public fields"),
+      {
+        expect_true(field %in% region_public_fields)
+      }
+    )
+  }
+  return(0)
+}
+
+check_all_country_fields <- function() {
+  avalibale_country_data <- get_available_datasets()
+  avaliable_sources <- avalibale_country_data$country
+  for (country in avaliable_sources) {
+    regionClass <- get(country)
+    check_country_fields(regionClass)
+  }
+  return(0)
+}
+
+check_country_methods <- function(regionClass) {
+  expected_public_methods <- names(CountryTemplate$public_methods)
+  region_public_methods <- names(regionClass$public_methods)
+  for (method in expected_public_methods) {
+    test_that(
+      paste("Expect", method, "is in", class(regionClass)[1], "public methods"),
+      {
+        expect_true(method %in% region_public_methods)
+      }
+    )
+  }
+  return(0)
+}
+
+check_all_country_methods <- function() {
+  avalibale_country_data <- get_available_datasets()
+  avaliable_sources <- avalibale_country_data$country
+  for (country in avaliable_sources) {
+    regionClass <- get(country)
+    check_country_methods(regionClass)
+  }
+  return(0)
+}
+
+# run tests
+check_all_country_fields()
+check_all_country_methods()
---
+++ tests/testthat/test-check_country_data.R
@@ -0,0 +1,41 @@
+check_clean_cols <- function(level, name, data) {
+  test_that(
+    paste("Expect column 'date' for", name, "at level", level, "is of 'Date' type"),
+    {
+      expect_s3_class(data[["date"]], "Date")
+    }
+  )
+}
+
+check_processed_cols <- function(level, name, data) {
+  test_that(
+    paste("Expect", name, "at level", level, "has correct colum types"),
+    {
+      expect_s3_class(data[["date"]], "Date")
+      expect_type(data[["cases_new"]], "double")
+      expect_type(data[["cases_total"]], "double")
+      expect_type(data[["deaths_new"]], "double")
+      expect_type(data[["deaths_total"]], "double")
+    }
+  )
+}
+
+check_all_country_cols <- function(level) {
+  avalibale_country_data <- get_available_datasets()
+  avaliable_sources <- dplyr::filter(
+    avalibale_country_data,
+    grepl(
+      "[A-Z][a-z]",
+      country
+    )
+  )
+  countries <- avaliable_sources$country
+  for (country in countries) {
+    data <- get_regional_data(country, level = level, class = T, verbose = F)
+    check_clean_cols(level, country, data$region$clean)
+    check_processed_cols(level, country, data$region$processed)
+  }
+}
+
+check_all_country_cols(level = 1)
+check_all_country_cols(level = 2)
