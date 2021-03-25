7733ad7beb4a9a9e2b65f748415bea6d643a8bfe
Sam Abbott <s.e.abbott12@gmail.com>
2021-03-25 17:20:24 +0000

    all get_ tests passing

--- tests/testthat/custom_data/generate_data.R
+++ tests/testthat/custom_data/generate_data.R
@@ -3,6 +3,7 @@ source("tests/testthat/functions/replacePublicR6Method.R")
 # save level 1 mexico data for testing
 mexico <- covidregionaldata::Mexico$new(verbose = FALSE)
 mexico$download()
+mexico$data$raw <- dplyr::slice(mexico$data$raw, 1:1000)
 mexico$clean()
 mexico$process()
 replacePublicR6Method(mexico, "download", function() {
@@ -23,17 +24,23 @@ replacePublicR6Method(mexico, "download", function() {
 saveRDS(mexico, "tests/testthat/custom_data/mexico_level_2_snap.rds")
 
 # save a snapshot of the ECDC data
-ecdc <- get_national_data(source = "ecdc", steps = TRUE)
-ecdc$raw <- dplyr::slice(ecdc$raw, 1:1000)
-ecdc <- clean_regional(ecdc)
-ecdc <- process_regional(ecdc)
-ecdc <- return_regional(ecdc, steps = TRUE)
+ecdc <- ECDC$new(verbose = FALSE, steps = FALSE)
+ecdc$download()
+ecdc$data$raw <- dplyr::slice(ecdc$data$raw, 1:1000)
+ecdc$clean()
+ecdc$process()
+replacePublicR6Method(ecdc, "download", function() {
+  return(invisible(NULL))
+})
 saveRDS(ecdc, "tests/testthat/custom_data/ecdc.rds")
 
 # save a snapshot of the WHO data
-who <- get_national_data(source = "who", steps = TRUE)
-who$raw <- dplyr::slice(who$raw, 1:1000)
-who <- clean_regional(who)
-who <- process_regional(who)
-who <- return_regional(who, steps = TRUE)
+who <- WHO$new(verbose = FALSE)
+who$download()
+who$data$raw <- dplyr::slice(who$data$raw, 1:1000)
+who$clean()
+who$process()
+replacePublicR6Method(who, "download", function() {
+  return(invisible(NULL))
+})
 saveRDS(who, "tests/testthat/custom_data/who.rds")
