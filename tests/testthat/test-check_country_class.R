3c3192098518e94a4185c41b82a449d5938cabe1
Joe <palmer-2@hotmail.co.uk>
2021-03-26 13:13:38 +0000

    refactored code using purrr walk

--- tests/testthat/test-check_country_class.R
+++ tests/testthat/test-check_country_class.R
@@ -1,51 +1,43 @@
-check_country_fields <- function(regionClass) {
+check_fields <- function(field, region_class) {
+  test_that(
+    paste(
+      "Expect", field, "is in",
+      class(region_class)[1], "public fields"
+    ),
+    {
+      expect_true(field %in% names(region_class$public_fields))
+    }
+  )
+}
+
+check_methods <- function(method, region_class) {
+  test_that(
+    paste(
+      "Expect", method, "is in",
+      class(region_class)[1], "public methods"
+    ),
+    {
+      expect_true(method %in% names(region_class$public_methods))
+    }
+  )
+}
+
+check_country_class <- function(country) {
+  region_class <- get(country)
   expected_public_fields <- names(CountryTemplate$public_fields)
-  region_public_fields <- names(regionClass$public_fields)
-  for (field in expected_public_fields) {
-    test_that(
-      paste("Expect", field, "is in", class(regionClass)[1], "public fields"),
-      {
-        expect_true(field %in% region_public_fields)
-      }
-    )
-  }
-  return(0)
-}
-
-check_all_country_fields <- function() {
-  available_country_data <- get_available_datasets()
-  available_sources <- available_country_data$country
-  for (country in available_sources) {
-    regionClass <- get(country)
-    check_country_fields(regionClass)
-  }
-  return(0)
-}
-
-check_country_methods <- function(regionClass) {
   expected_public_methods <- names(CountryTemplate$public_methods)
-  region_public_methods <- names(regionClass$public_methods)
-  for (method in expected_public_methods) {
-    test_that(
-      paste("Expect", method, "is in", class(regionClass)[1], "public methods"),
-      {
-        expect_true(method %in% region_public_methods)
-      }
-    )
-  }
-  return(0)
-}
-
-check_all_country_methods <- function() {
-  available_country_data <- get_available_datasets()
-  available_sources <- available_country_data$country
-  for (country in available_sources) {
-    regionClass <- get(country)
-    check_country_methods(regionClass)
-  }
-  return(0)
+  purrr::walk(
+    expected_public_fields,
+    check_fields,
+    region_class
+  )
+  purrr::walk(
+    expected_public_methods,
+    check_methods,
+    region_class
+  )
 }
 
 # run tests
-check_all_country_fields()
-check_all_country_methods()
+available_country_data <- get_available_datasets()
+purrr::walk(available_country_data$country, check_country_class)
