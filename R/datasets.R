e813aa5f2778fcf57bfb7eebb74ea18da312d3b3
Joe <palmer-2@hotmail.co.uk>
2021-03-30 11:54:56 +0100

    Added documentation for region codes

---
+++ R/datasets.R
@@ -0,0 +1,18 @@
+#' Region Codes for Italy Dataset.
+#'
+#' @description Details of the region codes used for the Italy dataset.
+#' @return A nested tibble of region codes and related information.
+"italy_codes"
+
+#' Region Codes for Mexico Dataset.
+#'
+#' @description Details of the region codes used for the Mexico dataset.
+#' @return A nested tibble of region codes and related information.
+"mexico_codes"
+
+#' Region Codes for UK Dataset.
+#'
+#' @description The uk authority look table for providing region codes used for
+#' level 2 UK data.
+#' @return A tibble of region codes and related information.
+"uk_authority_lookup_table"
