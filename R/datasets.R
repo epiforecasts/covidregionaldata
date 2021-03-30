cd2ac1513843bb5555456b81375cf4e53c69d38a
Joe <palmer-2@hotmail.co.uk>
2021-03-30 11:30:59 +0100

    remove old files no longer needed.

--- R/datasets.R
+++
@@ -1,5 +0,0 @@
-#' Region Codes for Each Dataset.
-#'
-#' @description Details of the region codes used by each dataset.
-#' @return A nested tibble of region codes and related information.
-"region_codes"
