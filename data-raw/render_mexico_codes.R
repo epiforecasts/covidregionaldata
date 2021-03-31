4df88b2ca722f7c186352155d53dafd4e09244fd
kathsherratt <kathsherratt@gmail.com>
2021-03-31 15:42:23 +0100

    documentation
    including explanatory note on mexico region codes

--- data-raw/render_mexico_codes.R
+++ data-raw/render_mexico_codes.R
@@ -1,4 +1,8 @@
 # mexico region codes
+# For ISO-3166-2 codes, see: https://www.iso.org/obp/ui/#iso:code:3166:MX
+# ISO codes were matched by name to INEGI codes in source data
+# Note that Level 1 codes are the first 2 characters of Level 2 codes
+# Level 2 codes remain as provided in source data
 mexico_codes <- vroom::vroom("data-raw/mexico_codes.csv")
 mexico_codes <- tibble::tibble(
   level = "level_1_region",
