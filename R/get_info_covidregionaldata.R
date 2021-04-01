22b46d7e38586bdc6c50d865c97f1dec385d5579
Sam Abbott <s.e.abbott12@gmail.com>
2021-04-01 12:46:37 +0000

    fix issues and improve testing

--- R/get_info_covidregionaldata.R
+++ R/get_info_covidregionaldata.R
@@ -1,16 +1,18 @@
 #' Get available datasets
-#' 
-#' @description 
+#'
+#' @description
 #'  `r lifecycle::badge("deprecated")`
-#'  
+#'
 #'  This function is deprecated. Please use `get_available_datasets()` instead.
-#'  
+#'
 #' @keywords internal
-#' @import lifecycle deprecate_stop
+#' @importFrom lifecycle deprecate_warn
 get_info_covidregionaldata <- function() {
-  deprecate_warn(when = "0.9.0",
-                 what = "covidregionaldata::get_info_covidregionaldata()", 
-                 with = "covidregionaldata::get_available_datasets()")
+  deprecate_warn(
+    when = "0.9.0",
+    what = "covidregionaldata::get_info_covidregionaldata()",
+    with = "covidregionaldata::get_available_datasets()"
+  )
   data <- get_available_datasets()
   return(data)
 }
