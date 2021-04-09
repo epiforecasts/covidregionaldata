9d78e16b9544306c27361599e704b868aa1b419a
Joe <palmer-2@hotmail.co.uk>
2021-04-09 16:11:51 +0100

    fixed yaml mistake and updated shared methods to best method

--- R/shared-methods.R
+++ R/shared-methods.R
@@ -96,13 +96,13 @@ initialise_dataclass <- function(self, level = "1", regions,
     self$target_regions <- regions
   }
 
-  if (!is.null(self$level_data_urls[[self$level]])) {
+  if (length(self$common_data_urls) > 0) {
     self$data_urls <- merge(
       self$common_data_urls,
       self$level_data_urls[[self$level]]
     )
   } else {
-    self$data_urls <- self$common_data_urls
+    self$data_urls <- self$level_data_urls[[self$level]]
   }
 
   if (get) {
