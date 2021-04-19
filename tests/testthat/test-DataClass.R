9e974fe5ee36c226ef701a641d07f3fada768723
Sam Abbott <s.e.abbott12@gmail.com>
2021-04-19 15:44:46 +0000

    fix for test() check

--- tests/testthat/test-DataClass.R
+++ tests/testthat/test-DataClass.R
@@ -17,14 +17,13 @@ D <- R6::R6Class("D",
   inherit = DataClass,
   public = list(
     common_data_urls = list(
-      main = "tests/testthat/custom_data/ecdc.csv"
+      main = "custom_data/ecdc.csv"
     ),
     clean_common = function() {
       self$data$clean <- self$data$raw$main
     }
   )
 )
-
 d <- D$new(verbose = FALSE)
 
 test_that("An inherited DataClass class can be intialised", {
@@ -36,12 +35,14 @@ test_that("DataClass can download data", {
   expect_s3_class(d$data$raw$main, "data.frame")
   expect_equal(nrow(d$data$raw$main), 1000)
 })
+d$download()
 
 test_that("DataClass can clean data", {
   expect_error(d$clean(), NA)
   expect_s3_class(d$data$clean, "data.frame")
   expect_equal(d$data$clean, d$data$raw$main)
 })
+d$clean()
 
 test_that("DataClass can filter data", {
   expect_error(d$filter("Zambia"), NA)
@@ -49,19 +50,20 @@ test_that("DataClass can filter data", {
   expect_equal(unique(d$data$clean$level_1_region), "Zambia")
   expect_true(nrow(d$data$clean) != 0)
 })
+d$filter()
 
 test_that("DataClass can process data", {
-  expect_error(d$process(), NA)
+  expect_error(suppressMessages(d$process()), NA)
   expect_s3_class(d$data$processed, "data.frame")
   expect_true(nrow(d$data$processed) != 0)
 })
+suppressMessages(d$process())
 
 test_that("DataClass can return data", {
   expect_error(d$return(), NA)
   expect_s3_class(d$return(), "data.frame")
-  expect_true(nrow(d$data$return) != 0)
   d$steps <- TRUE
-  expect_equal(names(d$return()), c("raw", "clean", "return", "processed"))
+  expect_equal(names(d$return()), c("raw", "clean", "processed", "return"))
 })
 
 test_that("DataClass returns a summary", {
